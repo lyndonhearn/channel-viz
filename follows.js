0794e4feec94fa044fbf5586eb80def1c6332dd7
lyndonhearn <lyndonhearn@yahoo.co.uk>
2014-05-27 15:44:45 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,10 +15,10 @@
 	var defaultKey		= 'uAJE6ZzK79SoWfeW34JhoKPZHHaE59Kvro7Z8m7v98S9HHNH', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1010812644!temp_int!temp_ext!noisedetected!micvolume!load_avg], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // interval for data in seconds 10800 = 6 hours
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
